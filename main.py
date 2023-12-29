407d85210d1d76f97cfab47099e3086d670037e9
João Pantaleão <jopantaleao@gmail.com>
2023-12-29 22:02:44 +0000

    adding login and register

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectMultipleField, SelectField, BooleanField
+from wtforms import StringField, SubmitField, SelectMultipleField, SelectField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, URL
 import stripe
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretcode'
 Bootstrap5(app)
 
+# Configure Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # stripe configuration
 app.config["STRIPE_PUBLIC_KEY"] = "pk_test_51OSP3yJsLunIJogOr4E931yptIqQEJgSLEMpbdgymXlrZoTjokeYPIJuDEbUYYNSFKcNaXVJhiPse6nzKTjpR1mV00OkuvAHzp"
@@ -22,7 +26,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
 db = SQLAlchemy()
 db.init_app(app)
 
-
+#books database
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -37,8 +41,24 @@ class Books(db.Model):
         return f"<Book {self.title}>"
 
 
+# with app.app_context():
+#     db.create_all()
+
+#user database
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+
+
 with app.app_context():
     db.create_all()
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
 
 
 class AddForm(FlaskForm):
@@ -50,6 +70,14 @@ class AddForm(FlaskForm):
     price_code = StringField("Price Code", validators=[DataRequired()])
     stock = StringField("Stock", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+# Create a form to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
 
 
 @app.route('/', methods=["GET",'POST'])
@@ -98,6 +126,21 @@ def add():
         db.session.commit()
         return redirect("/")
     return render_template("add.html", form=form)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+
+        #check if the user is already in the database
+        result = db.session.execute(db.select(User).where(User.email == form.email.data))
+        user = result.scalar()
+        if user:
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
 
 
 @app.route("/thanks")
